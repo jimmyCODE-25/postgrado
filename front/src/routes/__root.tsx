a23313c49996bbf248fcea364bcddf4d4acc53d9
Jimmy CODE <jimmyvasquezmamani@gmail.com>
2025-10-18 02:04:37 -0400

    Agregar componente DialogoProveedor en el diseño raíz y actualizar estilos de fondo

--- front/src/routes/__root.tsx
+++ front/src/routes/__root.tsx
@@ -1,16 +1,17 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import { DialogoProveedor } from "@/components/ui/dialogo/DialogoProveedor";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import "../styles/globals.css";
 
 const RootLayout = () => (
-  <>
+  <DialogoProveedor>
     <Navbar />
-    <div className="bg-gradient-to-br from-gray-500 via-gray-300 to-gray-100 min-h-screen flex flex-col">
+    <div className="flex flex-col gap-4 bg-gradient-to-br from-slate-900 via-slate-800 to-blue-900 p-4">
       <Outlet />
     </div>
     <Footer />
-  </>
+  </DialogoProveedor>
 );
 
 export const Route = createRootRoute({ component: RootLayout });
